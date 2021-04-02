6c52677d79fbd1a7091d1fd292c9d96be1350831
Talita1996 <talita_mota@outlook.com>
2021-04-02 13:33:18 -0300

    serial port connection started

---
+++ public/index.js
@@ -0,0 +1,13 @@
+const socket = io.connect('http://localhost:3000')
+
+socket.on('connected', () => {
+  console.log('Socket Connected')
+})
+socket.on('disconnect', () => {
+  console.log('Socket Disconnected')
+})
+
+socket.on('data', data => {
+    console.log(data)
+  /*document.body.setAttribute('style', `background-color: hsl(${Math.round(data/3)}, 100%, 50%)`)*/
+})
